a2920a4ed6165298c04877feea6f0b6ce68ca63f
ayaovi <djayespoiro@gmail.com>
2017-07-26 05:54:13 +0200

    feat: Working.

---
+++ src/Class1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace src
+{
+  public static class Class1
+  {
+    public static IEnumerable<string> Parse(string input)
+    {
+      return input.ReplaceEncapsulator(Tuple.Create('\"', '\"'), Tuple.Create('[', ']'))
+                  .ReplaceAllBoundedChar('.', new[] { Tuple.Create('[', ']') }, '*')
+                  .Split(new[] { '.', '[', ']' }, StringSplitOptions.RemoveEmptyEntries)
+                  .Select(x => x.Replace('*', '.'));
+    }
+
+    public static IEnumerable<int> FindAll(this string lhs, char target)
+    {
+      return lhs.ToCharArray()
+      .Select((x, i) => new { character = x, index = i })
+      .Where(x => x.character == target)
+      .Select(x => x.index);
+    }
+
+    public static bool HasCharacterBounded(this string lhs, int charIndex, IEnumerable<Tuple<char, char>> encapsulators)
+    {
+      var encapsulatorsOpeners = encapsulators.Select(x => x.Item1);
+      var encapsulatorsTerminators = encapsulators.Select(x => x.Item2);
+
+      var characters = lhs.ToCharArray();
+
+      for (int i = charIndex; i >= 0; i--)
+      {
+        if (encapsulatorsTerminators.Contains(characters[i])) return false;
+        if (encapsulatorsOpeners.Contains(characters[i])) return true;
+      }
+      return false;
+    }
+
+    public static SqlObjectName StringToSqlObjectName(string input)
+    {
+      var s = Parse(input).ToArray();
+      var sqlObjectName = new SqlObjectName();
+
+      if (s.Length == 1)
+      {
+        sqlObjectName.ObjectName = s[0];
+      }
+      else if (s.Length == 2)
+      {
+        sqlObjectName.Schema = s[0];
+        sqlObjectName.ObjectName = s[1];
+      }
+      else if (s.Length == 3)
+      {
+        sqlObjectName.Database = s[0];
+        sqlObjectName.Schema = s[1];
+        sqlObjectName.ObjectName = s[2];
+      }
+      else if (s.Length == 4)
+      {
+        sqlObjectName.Server = s[0];
+        sqlObjectName.Database = s[1];
+        sqlObjectName.Schema = s[2];
+        sqlObjectName.ObjectName = s[3];
+      }
+      else
+      {
+        throw new Exception("too many attribute");
+      }
+      return sqlObjectName;
+    }
+
+    public static bool Contains(IEnumerable<SqlObjectName> objects, SqlObjectName target)
+    {
+      return objects.Contains(target);
+    }
+
+    public static string ReplaceAllBoundedChar(this string lhs, char target, IEnumerable<Tuple<char, char>> encapsulators, char replacement)
+    {
+      var indices = lhs.FindAll(target);
+      return string.Concat(lhs.ToArray()
+                              .Select((x, i) => new { character = x, index = i })
+                            .Select(x =>
+                            {
+                              var character = x.character;
+                              if (indices.Contains(x.index) && lhs.HasCharacterBounded(x.index, encapsulators))
+                              {
+                                character = replacement;
+                              }
+                              return character;
+                            })
+      );
+    }
+
+    public static string ReplaceEncapsulator(this string lhs, Tuple<char, char> oldEncapsulators, Tuple<char, char> newEncapsulators)
+    {
+      var characters = lhs.ToArray();
+      var oldEncapsulatorsOpenerIndices = lhs.FindAll(oldEncapsulators.Item1).ToArray();
+      var oldEncapsulatorsTerminatorIndices = lhs.FindAll(oldEncapsulators.Item2).ToArray();
+
+      if (oldEncapsulatorsOpenerIndices.Length != oldEncapsulatorsTerminatorIndices.Length)
+      {
+        var part = (oldEncapsulatorsOpenerIndices.Length > oldEncapsulatorsTerminatorIndices.Length) ? "missing closing" : "extra closing";
+        throw new Exception($"{part} encapsulator");
+      }
+
+      if (oldEncapsulators.Item1 == oldEncapsulators.Item2)
+      {
+        if (oldEncapsulatorsOpenerIndices.Length == oldEncapsulatorsTerminatorIndices.Length &&
+        oldEncapsulatorsOpenerIndices.Length % 2 == 0)
+        {
+          for (int i = 0; i < oldEncapsulatorsOpenerIndices.Length; i += 2)
+          {
+            characters[oldEncapsulatorsOpenerIndices[i]] = newEncapsulators.Item1;
+            characters[oldEncapsulatorsOpenerIndices[i + 1]] = newEncapsulators.Item2;
+          }
+          return string.Concat(characters);
+        }
+        else
+        {
+          var part = (oldEncapsulatorsOpenerIndices.Length > oldEncapsulatorsTerminatorIndices.Length) ? "missing closing" : "extra closing";
+          throw new Exception($"{part} encapsulator");
+        }
+      }
+
+      return string.Concat(characters.Select((x, i) => new { character = x, index = i })
+                                    .Select(x =>
+                                    {
+                                      var character = x.character;
+                                      if (oldEncapsulatorsOpenerIndices.Contains(x.index))
+                                        character = newEncapsulators.Item1;
+                                      else if (oldEncapsulatorsTerminatorIndices.Contains(x.index))
+                                        character = newEncapsulators.Item2;
+                                      return character;
+                                    })
+      );
+    }
+  }
+
+  public class SqlObjectName
+  {
+    public string Server { get; set; } = string.Empty;
+    public string Database { get; set; } = string.Empty;
+    public string Schema { get; set; } = "dbo";
+    public string ObjectName { get; set; } = string.Empty;
+
+    public override bool Equals(object obj)
+    {
+      var sqlObjectName = obj as SqlObjectName;
+      return ObjectName.ToLower() == sqlObjectName.ObjectName.ToLower() &&
+      Schema.ToLower() == sqlObjectName.Schema.ToLower() &&
+      Database.ToLower() == sqlObjectName.Database.ToLower() &&
+      Server.ToLower() == sqlObjectName.Server.ToLower();
+    }
+
+    public override string ToString()
+    {
+      var name = Schema + ObjectName;
+      if (!string.IsNullOrEmpty(Database)) name = Database + name;
+      if (!string.IsNullOrEmpty(Server)) name = Server + name;
+      return name;
+    }
+  }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using src;
+using Xunit;
+
+namespace test
+{
+  public class UnitTest1
+  {
+    [Theory]
+    [InlineData(new []{"dbo.SomeObject"}, "dbo.SomeObject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "dbo.Someobject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "dbo.someObject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "dbo.someobject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "SomeObject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "Someobject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "someObject", true)]
+    [InlineData(new []{"dbo.SomeObject"}, "someobject", true)]
+    [InlineData(new []{"SomeObject"}, "dbo.SomeObject", true)]
+    [InlineData(new []{"SomeObject"}, "dbo.Someobject", true)]
+    [InlineData(new []{"SomeObject"}, "dbo.someObject", true)]
+    [InlineData(new []{"SomeObject"}, "dbo.someobject", true)]
+    [InlineData(new []{"[dbo].SomeObject"}, "DBO.SoMeObjecT", true)]
+    [InlineData(new []{"[dbo].SomeObject"}, "dBo.SomEobject", true)]
+    [InlineData(new []{"[dbo].SomeObject"}, "dbO.someObject", true)]
+    [InlineData(new []{"[dbo].SomeObject"}, "Dbo.sOmeobJEct", true)]
+    [InlineData(new []{"\"dbo\".SomeObject"}, "[DBO].SoMeObjecT", true)]
+    [InlineData(new []{"[dbo].SomeObject"}, "\"dBo\".SomEobject", true)]
+    [InlineData(new []{"dbo.[SomeObject.MORE]"}, "[dbO].[someObject.more]", true)]
+    [InlineData(new []{"[dbo].\"SomeObject.MoRE\""}, "Dbo.[sOmeobJEct.more]", true)]
+    public void Contains_GivenObjectListAndTargetObject_ExpectResult(string[] strSqlObjectNames, string strTarget, bool expected)
+    {
+      //Arrange
+      var sqlObjectNames = strSqlObjectNames.Select(Class1.StringToSqlObjectName);
+      var target = Class1.StringToSqlObjectName(strTarget);
+
+      //Act && Assert      
+      Assert.True(expected == Class1.Contains(sqlObjectNames, target));
+    }
+
+    [Theory]
+    [InlineData("dbo.SomeObject.obj", new []{"dbo", "SomeObject", "obj"})]
+    [InlineData("[dbo].SomeObject.obj", new []{"dbo", "SomeObject", "obj"})]
+    [InlineData("dbo.[SomeObject].obj", new []{"dbo", "SomeObject", "obj"})]
+    [InlineData("[dbo].[SomeObject].obj", new []{"dbo", "SomeObject", "obj"})]
+    [InlineData("[dbo.something].SomeObject.obj", new []{"dbo.something", "SomeObject", "obj"})]
+    [InlineData("[dbo.something].[Some.Object.More].obj", new []{"dbo.something", "Some.Object.More", "obj"})]
+    [InlineData("\"dbo\".\"SomeObject\".obj", new []{"dbo", "SomeObject", "obj"})]
+    public void Parse_GivenDotDelimitedObjectName_ExpectListSeperatedByDot(string input, string[] expected)
+    {
+      //Arrange && Act
+      var result = Class1.Parse(input);
+      //Assert
+      Assert.True(AreEqual(expected, result));
+    }
+
+    [Theory]
+    [InlineData("[dbo].[SomeObject]", '.', new []{5})]
+    [InlineData("he.is.a.mad.dog", '.', new []{2, 5, 7, 11})]
+    [InlineData("he+is+a+mad.dog", '+', new []{2, 5, 7})]
+    public void FindAll_GivenString_ExpectAllIndicesOfSpecifiedCharacter(string input, char target, int[] expected)
+    {
+      //Arrange && Act
+      var result = input.FindAll(target);
+
+      //Assert
+      Assert.True(AreEqual(expected, result));
+    }
+
+    [Theory]
+    [InlineData("[dbo.something].[SomeObject]", 5, new []{"[]"}, true)]
+    [InlineData("dbo.something.SomeObject", 5, new []{"[]"}, false)]
+    [InlineData("[dbo.something].[SomeObject]", 15, new []{"[]"}, false)]
+    [InlineData("{dbo.something}.[SomeObject.x]", 5, new []{"[]", "{}"}, true)]
+    [InlineData("{dbo.something}.[SomeObject.x]", 15, new []{"[]", "{}"}, false)]
+    [InlineData("{dbo.something}.[SomeObject.x]", 25, new []{"[]", "{}"}, true)]
+    public void HasCharacterBounded_GivenString_ExpectResult(string input, int charIndex, string[] strEncapsulators, bool expected)
+    {
+      //Arrange
+      var encapsulators = strEncapsulators.Select(x => x.ToArray())
+      .Select(x => Tuple.Create(x[0], x[1]));
+
+      //Act && Assert
+      Assert.True(expected == input.HasCharacterBounded(charIndex, encapsulators));
+    }
+
+    [Theory]
+    [InlineData("[dbo.some].[Object.kfjf]", '.', '*', new []{"[]"}, "[dbo*some].[Object*kfjf]")]
+    [InlineData("dbo.some.Object.kfjf", '.', '*', new []{"[]"}, "dbo.some.Object.kfjf")]
+    public void ReplaceAllBoundedChar_GivenString_ExpectResult(string input, char target, char replacement, string[] strEncapsulators, string expected)
+    {
+      //Arrange
+      var encapsulators = strEncapsulators.Select(x => x.ToArray())
+                                          .Select(x => Tuple.Create(x[0], x[1]));
+
+      //Act && Assert
+      Assert.Equal(expected, input.ReplaceAllBoundedChar(target, encapsulators, replacement));
+    }
+
+    [Theory]
+    [InlineData("\"dbo.something\".\"SomeObject\"", "\"\"", "[]", "[dbo.something].[SomeObject]")]
+    [InlineData("{dbo.something}.SomeObject", "{}", "[]", "[dbo.something].SomeObject")]
+    [InlineData("{dbo.something}.<SomeObject>", "<>", "{}", "{dbo.something}.{SomeObject}")]
+    public void ReplaceEncapsulator_GivenString_ExpectResult(string input, string strOldEncapsulator, string strNewEncapsulator, string expected)
+    {
+      //Arrange
+      var oldEncapsulators = Tuple.Create(strOldEncapsulator.First(), strOldEncapsulator.Last());
+      var newEncapsulators = Tuple.Create(strNewEncapsulator.First(), strNewEncapsulator.Last());
+
+      //Act && Assert
+      Assert.True(expected == input.ReplaceEncapsulator(oldEncapsulators, newEncapsulators));
+    }
+
+    [Theory]
+    [InlineData("{dbo.something}.{SomeObject", "{}", "[]", "missing closing encapsulator")]
+    [InlineData("{dbo.something}.SomeObject}", "{}", "[]", "extra closing encapsulator")]
+    public void ReplaceEncapsulator_GivenString_ExpectException(string input, string strOldEncapsulator, string strNewEncapsulator, string exceptionMessage)
+    {
+      //Arrange
+      var oldEncapsulators = Tuple.Create(strOldEncapsulator.First(), strOldEncapsulator.Last());
+      var newEncapsulators = Tuple.Create(strNewEncapsulator.First(), strNewEncapsulator.Last());
+
+      //Act
+      var exception = Assert.Throws<Exception>(() => {input.ReplaceEncapsulator(oldEncapsulators, newEncapsulators);});
+
+      //Assert      
+      Assert.Equal(exceptionMessage, exception.Message);
+    }
+
+    private bool AreEqual<T>(IEnumerable<T> expected, IEnumerable<T> result)
+    {
+      Assert.True(expected.Count() == result.Count());
+      Assert.True(expected.All(x => result.Contains(x)));
+      return true;
+    }
+  }
+}
